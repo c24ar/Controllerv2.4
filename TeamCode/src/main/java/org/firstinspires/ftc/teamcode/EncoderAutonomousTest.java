ba6879dddc1a90476fa9ec4dfd793d7afeb6dc64
c24ar <72398535+c24ar@users.noreply.github.com>
2021-11-10 10:52:49 -0500

    Update and rename EncoderAutonomousTest to EncoderAutonomousTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutonomousTest.java
@@ -107,15 +107,7 @@ public class DriveWithEncoder extends LinearOpMode
         FrontRight.setPower(-0.25);
         BackRight.setPower(-0.25);
 
-        while (opModeIsActive() && FrontLeft.getCurrentPosition() > frontLeft.getTargetPosition())
-        {
-            telemetry.addData("encoder-front-left", FrontLeft.getCurrentPosition());
-            telemetry.addData("encoder-back-left", BackLeft.getCurrentPosition());
-            telemetry.addData("encoder-front-right", FrontRight.getCurrentPosition());
-            telemetry.addData("encoder-back-right", BackRight.getCurrentPosition());
-            telemetry.update();
-            idle();
-        }
+       
 
         // set motor power to zero to stop motors.
 
